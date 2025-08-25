20810def40bf6b2b5370593f8cabd6eea8a885cb
sofiamellano <mellanosofia1@gamail.com>
2025-08-25 17:24:13 -0300

    Añadir GeminiController y pruebas unitarias
    Se ha implementado un nuevo controlador `GeminiController` para realizar solicitudes a una API externa y generar contenido basado en un texto proporcionado.
    
    Se ha creado un nuevo proyecto de prueba `BiblioTestProject` con pruebas unitarias para verificar la funcionalidad del controlador.
    
    Se ha definido la interfaz `IGenimiService` y se ha añadido una clase `GeminiService`, aunque esta última aún no está implementada.
    
    Se han reorganizado y simplificado las estructuras de carpetas en `Service.csproj` y `Backend.csproj`, y se han habilitado configuraciones como `ImplicitUsings` y `Nullable` para mejorar la gestión de tipos nulos.

---
+++ BiblioTestProject/UnitTestGemini.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Text.Json;
+
+namespace BiblioTestProject
+{
+    public class UnitTestGemini
+    {
+        [Fact]
+        public async Task TestObtenerResumenLibroIA()
+        {
+            //leemos la api key desde appsettings.json
+            var configuration = new ConfigurationBuilder()
+                  .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                  .AddEnvironmentVariables()
+                  .Build();
+
+            var apiKey = configuration["ApiKeyGemini"];
+            var url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key= " + apiKey;
+
+            var prompt = $"Me puedes dar un resumen de 100 palabras como máximo del libro Sin Red: Nadal, Federer y la historia detrás del duelo que cambió el tenis";
+
+            var payload = new
+            {
+                contents = new[]
+                {
+                    new
+                    {
+                        parts = new[]
+                        {
+                            new { text = prompt }
+                        }
+                    }
+                }
+            };
+
+            var json = JsonSerializer.Serialize(payload);
+            using var client = new HttpClient();
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(url, content);
+            var result = await response.Content.ReadAsStringAsync();
+
+            using var doc = JsonDocument.Parse(result);
+            var texto = doc.RootElement
+               .GetProperty("candidates")[0]
+               .GetProperty("content")
+               .GetProperty("parts")[0]
+               .GetProperty("text")
+               .GetString();
+
+            Console.WriteLine($"Respuesta de IA: {texto}");
+            Assert.True(response.IsSuccessStatusCode);
+        }
+    }
+}
